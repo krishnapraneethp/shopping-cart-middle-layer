9f2beaf1e67d7a4fae6fead6110599de8aafac1a
Krishna Praneeth Prodduturi <krishnapraneeth.p@gmail.com>
2019-07-14 01:02:20 +0530

    Initial commit

---
+++ src/main/java/com/shoppingcart/middlelayer/ShoppingCartMiddleLayerApplication.java
@@ -0,0 +1,13 @@
+package com.shoppingcart.middlelayer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingCartMiddleLayerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingCartMiddleLayerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shoppingcart/middlelayer/ShoppingCartMiddleLayerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shoppingcart.middlelayer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingCartMiddleLayerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
